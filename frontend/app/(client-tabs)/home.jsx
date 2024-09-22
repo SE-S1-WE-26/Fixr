18050ed35a08c207f99954e12d65bc3f95b56d07
Dinithi Rajapaksha <153760230+DinithiTR@users.noreply.github.com>
2024-09-22 22:55:08 +0530

    Updated home.jsx
    added scanner button

--- frontend/app/(client-tabs)/home.jsx
+++ frontend/app/(client-tabs)/home.jsx
@@ -1,4 +1,4 @@
-import { ScrollView } from "react-native";
+import { ScrollView, Touchable, TouchableOpacity, StyleSheet } from "react-native";
 import { React} from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import HomeHeader from "../../components/worker/home/HomeHeader";
@@ -6,6 +6,8 @@ import EOMCard from "../../components/worker/home/EOMCard";
 import TopWorkerList from "../../components/client/home/TopWorkerList";
 import PopualrCategoriesList from "../../components/client/home/PopularCategoriesList";
 import Heading from '../../components/common/Heading';
+import ScanIcon from "../../components/client/home/ScanIcon";
+import { icons } from "../../constants";
 
 const Home = () => {
   return (
@@ -18,8 +20,22 @@ const Home = () => {
         <Heading name="Popular Categories" link="/" />
         <PopualrCategoriesList />
       </ScrollView>
+
+      <TouchableOpacity style={styles.scanner}>
+        <ScanIcon icon={icons.scanner}/>
+      </TouchableOpacity>
+
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  scanner: {
+    position: "absolute",
+    bottom: 10, 
+    right: 15, 
+    zIndex: 10, 
+  },
+});
 
 export default Home;
