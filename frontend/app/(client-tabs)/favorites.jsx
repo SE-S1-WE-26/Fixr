e8dffe1b9cb1fca0ae904dcad14e779faa40a7aa
Dinithi Rajapaksha <153760230+DinithiTR@users.noreply.github.com>
2024-10-07 16:00:56 +0530

    update favourites.jsx

--- frontend/app/(client-tabs)/favorites.jsx
+++ frontend/app/(client-tabs)/favorites.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../components/common/Header";
+import PopularCategoriesList from "../../components/client/home/PopularCategoriesList";
+import SearchBar from "../../components/client/favourites/SearchBarComponent";
+import FavouriteWorkersGrid from "../../components/client/favourites/FavouriteWorkersGrid";
 
 const Favorites = () => {
   return (
     <SafeAreaView className="h-full bg-white">
       <ScrollView>
-        <View className="w-full h-full">
+        <View>
           <Header title={"Favorites"} />
         </View>
+        <PopularCategoriesList/>
+        <SearchBar/>
+        <FavouriteWorkersGrid/>
       </ScrollView>
     </SafeAreaView>
   );
