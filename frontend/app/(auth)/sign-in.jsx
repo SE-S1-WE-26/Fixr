858516119d44f2a178fe83875725d904a080a3de
Sandali Dias <ssd.dias29@gmail.com>
2024-10-09 03:42:36 +0530

    Downloaded packages

--- frontend/app/(auth)/sign-in.jsx
+++ frontend/app/(auth)/sign-in.jsx
@@ -19,7 +19,7 @@ const SignIn = () => {
     setErrorMessage(""); // Reset error message
   
     try {
-        const response = await axios.post('http://192.168.1.3:8080/user/signin', {
+        const response = await axios.post('http://192.168.8.103:8010/user/signin', {
             username: form.email, // Use form.email for username
             password: form.password
         });
